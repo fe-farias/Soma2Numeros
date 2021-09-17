cab111c45263754c9c63027d45f65a3d23c0fd16
Felipe Francisco <fefarias97@gmail.com>
2021-09-17 17:31:26 -0300

    Resolução do exercício Soma2Numeros

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1,n2;
+            string numeroDigitado;
+
+            Console.Write("Digite o primeiro valor: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Digite o segundo valor: ");
+            numeroDigitado = Console.ReadLine();
+            n2 = Convert.ToDouble(numeroDigitado);
+
+            double resultado = n1 + n2;
+
+            Console.WriteLine($"Soma: {resultado}");
+
+
+        }
+    }
+}
